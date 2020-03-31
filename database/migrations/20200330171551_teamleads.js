ef896482eb50a368156899b596fc01beb0131e7f
Robert Gordon <Komeuppance@gmail.com>
2020-03-30 17:21:30 -0700

    Added migration TLs

---
+++ database/migrations/20200330171551_teamleads.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("teamleads", tbl => {
+      tbl.increments();
+      tbl
+        .string("email", 255)
+        .notNullable()
+        .unique();
+      tbl.string("first_name", 255).notNullable();
+      tbl.string("last_name", 255).notNullable();
+      tbl.string("type", 255).notNullable();
+      tbl.string("password", 255).notNullable();
+      tbl
+        .integer("cohort_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("cohorts")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists("teamleads");
+  };
