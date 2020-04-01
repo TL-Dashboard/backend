f01da13a32a300fb0cc7e8114bfe64c0636eb609
Robert Gordon <Komeuppance@gmail.com>
2020-04-01 01:38:52 -0700

    Added retros model/router

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ const studentsRouter = require('../models/students/students-router.js');
 const assignmentsRouter = require('../models/assignments/assignments-router.js');
 const attendanceRouter = require('../models/attendance/attendance-router.js');
 const gradesRouter = require('../models/grades/grades-router.js');
-
+const retrosRouter = require('../models/retros/retros-router.js');
 
 const server = express();
 
@@ -25,6 +25,7 @@ server.use('/api/students', verifyToken, studentsRouter);
 server.use('/api/assignments', verifyToken, assignmentsRouter);
 server.use('/api/attendance', verifyToken, attendanceRouter);
 server.use('/api/grades', verifyToken, gradesRouter);
+server.use('/api/retros', verifyToken, retrosRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Server is online!</h2><br><div>Visit: <a href='https://github.com/build-international-school/Back-End'>https://github.com/build-international-school/Back-End</a> for more API info.</div>`)
---
+++ models/retros/retros-model.js
@@ -0,0 +1,49 @@
+const db = require("../../database/dbConfig.js");
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findById,
+    update,
+    remove,
+};
+
+function add(record) {
+    return db("retros")
+        .insert(record, "id")
+        .then(ids => {
+            const [id] = ids;
+            return findById(id);
+        });
+}
+function find() {
+    return db("retros").select("*");
+}
+
+function findBy(filter) {
+    console.log('filter', filter)
+    return db("retros")
+        .where(filter)
+        .orderBy('id', 'desc');
+}
+
+function findById(id) {
+    return db("retros")
+        .select("*")
+        .where({ id })
+        .first();
+}
+
+function update(changes, id) {
+    return db('retros')
+      .where({ id })
+      .update(changes)
+      .select("*");
+  }
+
+function remove(id) {
+    return db('retros')
+      .where('id', id)
+      .del();
+  }
